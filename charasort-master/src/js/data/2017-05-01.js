3c6b9f936d0e01e39101bdb55fd4be191c72afde
roberttaylorV <110215833+roberttaylorV@users.noreply.github.com>
2022-10-26 00:26:53 -0500

    Add files via upload
    Added all NFL sorter files

---
+++ charasort-master/src/js/data/2017-05-01.js
@@ -0,0 +1,242 @@
+dataSetVersion = "2022-10-25"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Division",
+    key: "stage",
+    tooltip: "Check this to restrict to characters that appear in certain stages as enemies.",
+    checked: false,
+    sub: [
+      { name: "NFC", key: "NFC" },
+      { name: "AFC", key: "AFC" },
+    ]
+  },
+  {
+    name: "Remove PC-98 Duplicates",
+    key: "pc98",
+    tooltip: "Check this to remove PC-98 characters with a Windows counterpart."
+  },
+  {
+    name: "Remove Non-Girls",
+    key: "notgirl",
+    tooltip: "Check this to remove all non-female characters."
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Seahawks",
+    img: "jSb56km.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "49ers",
+    img: "29M9oRp.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Rams",
+    img: "wjcwQeX.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Cardinals",
+    img: "nr1kLTz.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Chargers",
+    img: "u7vCaly.png",
+    opts: {
+    
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Raiders",
+    img: "saGwNzN.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Broncos",
+    img: "O95iBGW.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Cheifs",
+    img: "Qm9t5aT.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Vikings",
+    img: "4xu9F7k.png",
+    opts: {
+      
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Packers",
+    img: "O0IZiXr.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Lions",
+    img: "L1E8O0o.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Bears",
+    img: "5X4cxTH.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Browns",
+    img: "OoqTNYu.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Steelers",
+    img: "08Lmj15.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Bengals",
+    img: "kLEGFA9.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Ravens",
+    img: "RPSkrG5.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Cowboys",
+    img: "u40xQNT.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Eagles",
+    img: "2Pqz1r4.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Giants",
+    img: "r3kprJG.png",
+    opts: {
+    
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Commanders",
+    img: "btqAYci.png",
+    opts: {
+         
+      stage: ["NFC"]
+    }
+  },
+  {
+    name: "Patriots",
+    img: "DUSsy2u.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Bills",
+    img: "HOEQPtM.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Dolphins",
+    img: "rFVU26K.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Jets",
+    img: "y8a7iTJ.png",
+    opts: { stage: ["AFC"] }
+  },
+  {
+    name: "Texans",
+    img: "EILw7Tu.png",
+    opts: {  stage: ["AFC"] }
+  },
+  {
+    name: "Colts",
+    img: "speEHiO.png",
+    opts: {  stage: ["AFC"] }
+  },
+  {
+    name: "titans",
+    img: "WU1tOhD.png",
+    opts: {  stage: ["AFC"] }
+  },
+  {
+    name: "Jaguars",
+    img: "AKa9DRX.png",
+    opts: {
+      stage: ["AFC"]
+    }
+  },
+  {
+    name: "Falcons",
+    img: "prCE84h.png",
+    opts: { stage: ["NFC"] }
+  },
+  {
+    name: "Panthers",
+    img: "f7CxKBG.png",
+    opts: { stage: ["NFC"] }
+  },
+  {
+    name: "Buccaneers",
+    img: "q5hBoiy.png",
+    opts: {  stage: ["NFC"] }
+  },
+  {
+    name: "Saints",
+    img: "HBRypk6.png",
+    opts: {
+      stage: ["NFC"]
+    }
+  }
+];
